55a77b8a3ddacd4f77787ee34331c4f9be2b3eba
nztan <naizhao.tan@outlook.com>
2026-04-23 23:23:59 -0600

    add readonly user

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,1 +1,6 @@
 CREATE SCHEMA IF NOT EXISTS vehicle_dashboard;
+
+CREATE USER readonly_user WITH ENCRYPTED PASSWORD 'readonly123';
+GRANT CONNECT ON DATABASE vehicle_dashboard TO readonly_user;
+GRANT USAGE ON SCHEMA vehicle_dashboard TO readonly_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA vehicle_dashboard TO readonly_user;
