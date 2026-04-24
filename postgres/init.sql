88b72826dd6bc1ecb220d5e545feea649d06f841
nztan <naizhao.tan@outlook.com>
2026-04-23 23:34:54 -0600

    make user info as env var, and fix the readonly user access

--- postgres/init.sql
+++ postgres/init.sql
@@ -4,3 +4,6 @@ CREATE USER readonly_user WITH ENCRYPTED PASSWORD 'readonly123';
 GRANT CONNECT ON DATABASE vehicle_dashboard TO readonly_user;
 GRANT USAGE ON SCHEMA vehicle_dashboard TO readonly_user;
 GRANT SELECT ON ALL TABLES IN SCHEMA vehicle_dashboard TO readonly_user;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA vehicle_dashboard
+GRANT SELECT ON TABLES TO readonly_user;
